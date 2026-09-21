d12ca3a410d216d8268d3dee195ec41fc664ef63
Diptesh8 <shiva14420065@gmail.com>
2026-09-21 17:11:02 +0530

    added pentagonArea cacculation part in javascript file

---
+++ pentagon.js
@@ -0,0 +1,14 @@
+function pentagonCalculation(){
+    const pentagonBase =document.getElementById('pentagon-p');
+    const pentagonValueBase= pentagonBase.value;
+    const Base = parseFloat(pentagonValueBase);
+    // height section
+    const pentagonHeight =document.getElementById('pentagon-b');
+    const pentagonValueHeight= pentagonHeight.value;
+    const height = parseFloat(pentagonValueHeight);
+    // calculate section ------------
+    const area =0.5 * Base * height;
+//    show the final output in ui ---------
+const finalOutput =document.getElementById('pentagonArea');
+finalOutput.innerText=area;
+}
