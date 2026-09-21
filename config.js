1f1753146707ce8044e5532639baa25e32e58281
Diptesh8 <shiva14420065@gmail.com>
2026-09-21 07:35:35 +0530

    added tringle calculate logic

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
 function calculateTringleArea(){
-    console.log('inside function');
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const tringleBaseText = triangleBaseInput.value;
+    // console.log(tringleBaseText);
+    const base = parseFloat(tringleBaseText);
+    const tringleHeightInput =document.getElementById('tringle-height');
+    const tringleHeightText =tringleHeightInput.value;
+    // console.log(tringleHeightText);
+        const height = parseFloat(tringleHeightText);
+
+    const area= 0.5 * base * height;
+    console.log(area);
+
 }
